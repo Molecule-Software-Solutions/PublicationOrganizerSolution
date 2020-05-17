525eab599cbbc990775f0ee7fa09ef4f842b6f4f
Joshua Santiago <joshman1019@gmail.com>
2020-05-17 18:11:51 -0400

    Additional code cleanup and commenting

--- PublicationOrganizerUI/Helper Extensions/ApplicationPageTypeHelper.cs
+++ PublicationOrganizerUI/Helper Extensions/ApplicationPageTypeHelper.cs
@@ -5,6 +5,11 @@ namespace PublicationOrganizerUI
 {
     public static class ApplicationPageTypeHelper
     {
+        /// <summary>
+        /// Returns a UI page based on the <see cref="PageTypes"/> enum value passed into a viewmodel
+        /// </summary>
+        /// <param name="page"></param>
+        /// <returns></returns>
         public static Page GetApplicationPage(this PageTypes page)
         {
             switch (page)
